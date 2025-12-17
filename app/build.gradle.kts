7bb8ceeb69e8ed3faacb8cd7ab0cd1c5d05044cf
sebastien-riou <sriou@nimp.co.uk>
2025-12-17 23:02:05 +0100

    v0.0.13 make sendUrl multiline

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "me.leansys.smsfwd"
         minSdk = 24
         targetSdk = 36
-        versionCode = 12
-        versionName = "v0.0.12"
+        versionCode = 13
+        versionName = "v0.0.13"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
